3b31363332f812de37a1589fdce2cce04af48be2
ASPADeveloper <gitlab.developer@aspaconsulting.com>
2026-04-20 16:29:58 +0200

    Comienzo de scenes

--- src/main/java/com/therapai/Main.java
+++ src/main/java/com/therapai/Main.java
@@ -1,8 +1,17 @@
 package com.therapai;
 
+import com.therapai.utils.SceneManager;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage){
+        SceneManager.getInstance().setMainStage(primaryStage);
+        SceneManager.getInstance().switchTo("login.fxml");
+    }
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
--- src/main/java/com/therapai/controllers/LoginController.java
+++ src/main/java/com/therapai/controllers/LoginController.java
@@ -1,4 +1,14 @@
 package com.therapai.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class LoginController {
+    @FXML
+    public void login() {
+    }
+
+    @FXML
+    public void goRegister() {
+    }
 }
