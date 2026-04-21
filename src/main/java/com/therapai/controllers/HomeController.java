54d5e0772df0f4b9521f52c2407ce2fd978e21c2
ASPADeveloper <gitlab.developer@aspaconsulting.com>
2026-04-21 17:15:15 +0200

    Pantalla Login, Register, Home y Chat

---
+++ src/main/java/com/therapai/controllers/HomeController.java
@@ -0,0 +1,29 @@
+package com.therapai.controllers;
+
+import javafx.fxml.FXML;
+
+public class HomeController {
+    @FXML
+    public void newChat() {
+    }
+
+    @FXML
+    public void openChat() {
+    }
+
+    @FXML
+    public void goHome() {
+    }
+
+    @FXML
+    public void goHistory() {
+    }
+
+    @FXML
+    public void goSettings() {
+    }
+
+    @FXML
+    public void goProfile() {
+    }
+}
--- src/main/java/com/therapai/controllers/RegisterController.java
+++ src/main/java/com/therapai/controllers/RegisterController.java
@@ -1,4 +1,14 @@
 package com.therapai.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class RegisterController {
+    @FXML
+    public void register() {
+    }
+
+    @FXML
+    public void goLogin(){
+    }
 }
